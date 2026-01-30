92c73db11e762188f1dff944995a16563b173fb8
sobbingtofu <kukubasak@naver.com>
2026-01-30 19:20:00 +0900

    Update : 메인섹션 데스크탑 반응형 구현

--- src/sections/MainSection/MainSection.tsx
+++ src/sections/MainSection/MainSection.tsx
@@ -5,10 +5,19 @@ function MainSection() {
   return (
     <section id="MainSection" className="w-full h-screen overflow-auto bg-background flex items-center justify-center">
       <div className="flex-1">
-        <div className="2xl:w-[1520px] w-[1200px] h-[780px] mx-auto bg-background pt-[20px] pb-[35px] px-[50px] border">
+        <div
+          className="2xl:w-[1520px] w-[1200px] h-[780px] mx-auto bg-background 
+          2xl:pt-[20px] pt-[45px] pb-[35px] px-[50px] border"
+        >
           {/* 서브텍스트 및 이미지 */}
-          <div className="flex h-[180px] ml-auto justify-between items-start mr-[180px] w-[65%]">
-            <div className="flex flex-col font-[400] text-[15px] tracking-[-0.02em] leading-[1.7] space-y-[-3px] ">
+          <div
+            className="flex ml-auto justify-between items-start 
+            2xl:w-[65%] w-[70%] 2xl:mr-[180px] mr-[130px] 2xl:h-[180px] h-[160px]"
+          >
+            <div
+              className="flex flex-col 2xl:text-[15px] text-sm
+              font-[400] tracking-[-0.02em] leading-[1.7] space-y-[-3px] "
+            >
               <p>B2B 솔루션에서 요구사항 분석부터 기능 설계 및 개발,</p>
               <p>테스트 및 배포, 운영까지 전체 사이클을 경험했고,</p>
               <p>React와 Next.js 등 Modern FE 기술을 주도적으로</p>
@@ -17,16 +26,17 @@ function MainSection() {
               <p className="font-[600]">프로덕트를 완성하는 개발자를 지향합니다.</p>
             </div>
 
-            <div className="h-full sm:aspect-[3/4] aspect-[5/6] relative">
+            <div className="h-full aspect-[3/4] aspect-[5/6] relative">
               <Image src="/img/meFinal.png" alt="프로필" fill className="object-cover" priority />
             </div>
           </div>
           {/* 헤더 텍스트 및 강조점 */}
-          <div className="w-full flex items-end mt-[25px]">
+          <div className="w-full flex items-end 2xl:mt-[25px] mt-[35px]">
             {/* 헤더 텍스트 */}
             <div
-              className="flex flex-col items-end ml-auto w-[87%]
-              text-[85px] tracking-[-0.02em] leading-[1.3] space-y-[-3px] font-[500]"
+              className="flex flex-col items-end ml-auto w-full
+              2xl:text-[85px] text-[68px]
+              tracking-[-0.02em] leading-[1.3] space-y-[-3px] font-[500]"
             >
               <h2>
                 프로덕트의 <span className="font-[700]">시작</span>부터 <span className="font-[700]">끝</span>까지,
@@ -34,17 +44,20 @@ function MainSection() {
               <h2>
                 최적의 해답을 <span className="font-[700]">고민</span>하고 <span className="font-[700]">구현</span>하는
               </h2>
-              <h2 className="mr-[-28px]">
+              <h2 className="2xl:mr-[-28px] mr-[-25px]">
                 프론트엔드 개발자 <span className="font-[700]">윤기준</span>입니다.
               </h2>
             </div>
             {/* 강조점 */}
-            <div className="w-[120px] ml-[60px] aspect-square relative">
+            <div
+              className="2xl:w-[120px] 2xl:ml-[60px] w-[90px] ml-[40px]
+              aspect-square relative"
+            >
               <Image src="/img/star.png" alt="프로필" fill className="object-cover" priority />
             </div>
           </div>
           {/* 기술 카드 */}
-          <div className="mt-[65px] ml-auto items-start mr-[180px] w-[65%]">
+          <div className="2xl:mt-[65px] mt-[75px] ml-auto items-start 2xl:mr-[180px] mr-[130px] 2xl:w-[65%] w-[70%]">
             <SkillStackCardContainer animateSkillStackArea={true} />
           </div>
         </div>
