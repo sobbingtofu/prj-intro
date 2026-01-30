77e927640862b59dd3898122add853e289400415
sobbingtofu <kukubasak@naver.com>
2026-01-31 00:53:50 +0900

    Feat : 강조점 관련 로직 커스텀 훅으로 분리

--- src/sections/MainSection/MainSection.tsx
+++ src/sections/MainSection/MainSection.tsx
@@ -1,58 +1,17 @@
 "use client";
 
-import {useState, useRef} from "react";
 import SkillStackCardContainer from "@/src/components/SkillStackCardContainer/SkillStackCardContainer";
 import Image from "next/image";
 import useAnimateMainSection from "@/src/hooks/useAnimateMainSection/useAnimateMainSection";
 import {mainSectionRef} from "@/src/store/refStore";
+import useAnimateSpinningStar from "@/src/hooks/useAnimateSpinningStar/useAnimateSpinningStar";
 
 function MainSection() {
   const {animateMainText, animateStarIcon, animateSubText, animateImage, animateSkillStackArea} = useAnimateMainSection(
     {mainSectionRef},
   );
 
-  const [isStarClicked, setIsStarClicked] = useState(false);
-  const clickTimerRef = useRef<NodeJS.Timeout | null>(null);
-  const starRef = useRef<HTMLDivElement>(null);
-
-  // 애니메이션 속도 업데이트
-  const updateAnimationSpeed = (playbackRate: number) => {
-    if (starRef.current) {
-      const animations = starRef.current.getAnimations();
-      console.log("애니메이션 개수:", animations.length, "목표 속도:", playbackRate);
-      if (animations.length > 0) {
-        animations.forEach((animation) => {
-          animation.playbackRate = playbackRate;
-          console.log("애니메이션 속도 변경됨:", animation.playbackRate);
-        });
-      }
-    }
-  };
-
-  const handleStarClick = () => {
-    if (clickTimerRef.current) {
-      clearTimeout(clickTimerRef.current);
-    }
-    setIsStarClicked(true);
-    updateAnimationSpeed(30);
-    clickTimerRef.current = setTimeout(() => {
-      setIsStarClicked(false);
-      updateAnimationSpeed(1);
-    }, 1500);
-  };
-
-  const handleMouseEnter = () => {
-    if (!isStarClicked) {
-      updateAnimationSpeed(3);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    setIsStarClicked(false);
-    if (!isStarClicked) {
-      updateAnimationSpeed(1);
-    }
-  };
+  const {starRef, handleStarClick, handleMouseEnter, handleMouseLeave} = useAnimateSpinningStar();
 
   return (
     <section
