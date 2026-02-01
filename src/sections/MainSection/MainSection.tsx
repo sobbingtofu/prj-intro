b0bab95077ad72107e97be070457b11ffa0acbd9
sobbingtofu <kukubasak@naver.com>
2026-02-02 01:15:02 +0900

    Update : MainSection 반응형 개선

--- src/sections/MainSection/MainSection.tsx
+++ src/sections/MainSection/MainSection.tsx
@@ -32,17 +32,19 @@ function MainSection() {
       >
         {/* 실제 내용물 컨테이너 : sm 이상 */}
         <div
-          className="hidden sm:block
-          2xl:w-[1520px] w-[1200px] h-[780px] bg-background
+          className="hidden sm:block border
+          2xl:max-w-[2000px] 2xl:w-[1520px] h-[780px] bg-background
+          xl:max-w-[1200px] xl:min-w-[1150px]
+          max-w-[1150px] min-w-[945px]
           2xl:pt-[20px] pt-[45px] pb-[35px] px-[50px]"
         >
           {/* 서브텍스트 및 이미지 */}
           <div
             className="flex ml-auto justify-between items-start 
-            2xl:w-[65%] w-[70%] 2xl:mr-[180px] mr-[130px] 2xl:h-[180px] h-[160px]"
+            2xl:w-[65%] w-[70%] 2xl:mr-[180px] xl:mr-[130px] mr-[100px] 2xl:h-[180px] h-[160px]"
           >
             <div
-              className={`flex flex-col 2xl:text-[15px] text-sm
+              className={`flex flex-col 2xl:text-[15px] xl:text-sm text-[13px]
               font-[400] tracking-[-0.02em] leading-[1.7] space-y-[-3px]
               transition-all duration-700 ease-out
               ${animateSubText ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8"}`}
@@ -69,7 +71,7 @@ function MainSection() {
             {/* 헤더 텍스트 */}
             <div
               className={`flex flex-col items-end ml-auto w-full
-              2xl:text-[85px] text-[68px]
+              2xl:text-[85px] xl:text-[68px] text-[56px]
               tracking-[-0.02em] leading-[1.3] space-y-[-3px] font-[500]
               transition-all duration-700 ease-out
               ${animateMainText ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8"}`}
@@ -80,7 +82,7 @@ function MainSection() {
               <h2>
                 최적의 해답을 <span className="font-[700]">고민</span>하고 <span className="font-[700]">구현</span>하는
               </h2>
-              <h2 className="2xl:mr-[-28px] mr-[-25px]">
+              <h2 className="2xl:mr-[-28px] xl:mr-[-25px] mr-[-20px]">
                 프론트엔드 개발자 <span className="font-[700]">윤기준</span>입니다.
               </h2>
             </div>
@@ -90,7 +92,7 @@ function MainSection() {
               onClick={handleStarClick}
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}
-              className={`2xl:w-[120px] 2xl:ml-[60px] w-[90px] ml-[40px]
+              className={`2xl:w-[120px] 2xl:ml-[60px] xl:w-[90px] xl:ml-[40px] w-[70px] ml-[30px]
               aspect-square relative
               transition-all duration-700 ease-out
               animate-spin-and-bounce
@@ -101,7 +103,7 @@ function MainSection() {
             </div>
           </div>
           {/* 기술 카드 */}
-          <div className="2xl:mt-[65px] mt-[75px] ml-auto items-start 2xl:mr-[180px] mr-[130px] 2xl:w-[65%] w-[70%]">
+          <div className="2xl:mt-[65px] mt-[75px] ml-auto items-start 2xl:mr-[180px] xl:mr-[130px] mr-[100px] 2xl:w-[65%] w-[70%]">
             <SkillStackCardContainer animateSkillStackArea={animateSkillStackArea} />
           </div>
         </div>
